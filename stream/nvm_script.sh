8856431624faf734e247ea1ea65d081fafa47340
Svein Gunnar Fagerheim <sveingf@srl-mds2.cm.cluster>
2019-11-13 10:39:57 +0100

    Added Stream files to git

---
+++ stream/nvm_script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+socket1=0
+socket2=16
+num_threads=1
+file="test2.txt"
+#CPUs='{0}'
+CPUs=""
+
+echo NVM test > $file
+echo Socket 1 >> $file
+for (( n=0; n<16; n++ ))
+do
+	CPUs+="{$((socket1 + n))}"
+	export OMP_NUM_THREADS=$num_threads
+	echo OMP_NUM_THREADS=$num_threads >> $file
+	export export OMP_PLACES=$CPUs
+	echo export OMP_PLACES="'"$CPUs >> $file
+	./Stream_pmemobj_m.out | grep -E 'Copy:|Scale:|Add:|Triad:' | awk '{print $2}' >> $file
+	num_threads=$((num_threads+1))
+	CPUs+=","
+done
+
+num_threads=1
+CPUs=""
+
+echo Socket 1 >> $file
+
+for (( n=0; n<16; n++ ))
+do
+	CPUs+="{$((socket2 + n))}"
+        export OMP_NUM_THREADS=$num_threads
+        echo OMP_NUM_THREADS=$num_threads >> $file
+        export export OMP_PLACES=$CPUs
+        echo export OMP_PLACES="'"$CPUs"'" >> $file
+        ./Stream_pmemobj_m.out | grep -E 'Copy:|Scale:|Add:|Triad:' | awk '{print $2}' >> $file
+        num_threads=$((num_threads+1))
+        CPUs+=","	
+done
+
+#./Stream_c_s.out
+#export OMP_NUM_THREADS=1
+#export OMP_PLACES='{0}'
+
