ad1fb9b5d838248a46e184a559a599bd87d1a1ae
Svein Gunnar Fagerheim <sveingf@srl-mds1.cm.cluster>
2020-11-19 15:58:13 +0100

    updates

--- pmem-tester/Pmemobj_openMP_singelSocket.sh
+++ pmem-tester/Pmemobj_openMP_singelSocket.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #Pmemobj_openMP_singelSocket.sh
-file1="Result/Raws/NVM-NVM_8t.txt"
-file2="Result/Raws/DRAM-NVM_8t.txt"
-file3="Result/Raws/NVM-DRAM_8t.txt"
+file1="Result/Raws/NVM-NVM_16t.txt"
+file2="Result/Raws/DRAM-NVM_16t.txt"
+file3="Result/Raws/NVM-DRAM_16t.txt"
 
-iterations=1000
-total_threads=8
-socket1=0
-socket2=16
-CPUs={0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15}
+iterations=10
+total_threads=16
+#socket1=0
+#socket2=16
+#CPUs={0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15}
 
 #export OMP_PLACES=cores
 #export OMP_PROC_BIND=close
@@ -18,31 +18,31 @@ CPUs={0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15}
 #NVM-NVM
 echo NVM-NVM
 truncate -s 0 $file1
-for (( n=1; n<8; n++ ))
+for (( n=1; n<16; n++ ))
 do
 	echo $n
 	echo $n NVM thread >> $file1
-	numactl --physcpubind=0,2,4,6,8,10,12,14 ./Pmemobj_openMP_NVM-NVM.out $n $total_threads $iterations >> $file1
+	numactl --physcpubind=0-15 ./Pmemobj_openMP_NVM-NVM.out $n $total_threads $iterations >> $file1
 done
 
 #DRAM-NVM
 echo DRAM-NVM
 truncate -s 0 $file2
-for (( n=1; n<8; n++ ))
+for (( n=1; n<16; n++ ))
 do
 	echo $n
         echo $n >> $file2
-	numactl --physcpubind=0,2,4,6,8,10,12,14 ./Pmemobj_openMP_DRAM-NVM.out $n $total_threads $iterations >> $file2
+	numactl --physcpubind=0-15 ./Pmemobj_openMP_DRAM-NVM.out $n $total_threads $iterations >> $file2
 done
 
 #NVM-DRAM
 echo NVM-DRAM
 truncate -s 0 $file3
-for (( n=1; n<8; n++ ))
+for (( n=1; n<16; n++ ))
 do
 	echo $n
 	echo $n >> $file3
-	numactl --physcpubind=0,2,4,6,8,10,12,14 ./Pmemobj_openMP_NVM-DRAM.out $n $total_threads $iterations >> $file3
+	numactl --physcpubind=0-15 ./Pmemobj_openMP_NVM-DRAM.out $n $total_threads $iterations >> $file3
 done
 
 
--- stream/DRAM_test.sh
+++ stream/DRAM_test.sh
@@ -2,20 +2,56 @@
 socket1=0
 socket2=16
 num_threads=1
-file="temp.txt"
+file1="temp.txt"
+file2="DRAM_test_32.txt"
 CPUs=""
 #program="Stream_.out"
 program="Stream_c_m.out"
+cores=0
 
-echo DRAM test > $file
+if [ -f $file1 ]; then
+   rm temp.txt
+fi
+
+if [ -f $file2 ]; then
+   rm DRAM_test_32.txt
+fi
+
+echo DRAM test cpu 0-32 > $file2
 
 for (( n=0; n<16; n++ ))
 do
-	echo $n
-	echo $n >> $file
-	numactl --physcpubind=0-$n ./$program | grep -E 'Copy:|Scale:|Add:|Triad:' | awk '{print $2}' >> $file
-
+	echo $(($n+1))
+	echo $(($n+1)) >> $file1
+	numactl --physcpubind=0-$n ./$program | grep -E 'Copy:|Scale:|Add:|Triad:' | awk '{print $2}' >> $file1
 done
+
+for (( n=17; n<33; n++ ))
+do
+	export OMP_NUM_THREADS=$n
+	echo $n
+	echo $n >> $file1
+	numactl --physcpubind=0-15 ./$program | grep -E 'Copy:|Scale:|Add:|Triad:' | awk '{print $2}' >> $file1
+done
+
+m=0
+templine=""
+
+while IFS= read -r line
+do
+	if (( $m == 0 )) ; then
+		templine=$line 
+		m=$(($m+1))
+	elif (( $m == 4 )) ; then
+		templine=$templine","$line
+		echo $templine >> $file2
+		templine=""
+		m=0
+	else
+		templine=$templine","$line
+		m=$(($m+1))
+	fi
+done < "$file1"
 
 #./Stream_c_s.out
 #export OMP_NUM_THREADS=1
