cf034f1800a70b2bc10bf653c6a8ca422122f094
Svein Gunnar Fagerheim <sveingf@srl-mds2.cm.cluster>
2021-03-27 13:06:12 +0100

    updates

--- stream/DRAM_average_test.sh
+++ stream/DRAM_average_test.sh
@@ -3,36 +3,36 @@ socket1=0
 socket2=16
 num_threads=1
 file1="temp.txt"
-file2="DRAM_average_test_32.txt"
+file2="DRAM_average_test_16.txt"
 CPUs=""
 #program="Stream_.out"
 program="Stream_average.out"
 cores=0
 
 if [ -f $file1 ]; then
-   rm temp.txt
+   rm $file1
 fi
 
 if [ -f $file2 ]; then
-   rm DRAM_average_test_32.txt
+   rm $file2
 fi
 
 echo DRAM average test cpu 0-32 > $file2
 
-for (( n=0; n<16; n++ ))
+for (( n=15; n>=0; n-- ))
 do
 	echo $(($n+1))
 	echo $(($n+1)) >> $file1
 	numactl --physcpubind=0-$n ./$program | grep -E 'Copy:|Scale:|Add:|Triad:|Average:' | awk '{print $2}' >> $file1
 done
 
-for (( n=17; n<33; n++ ))
-do
-	export OMP_NUM_THREADS=$n
-	echo $n
-	echo $n >> $file1
-	numactl --physcpubind=0-15 ./$program | grep -E 'Copy:|Scale:|Add:|Triad:|Average:' | awk '{print $2}' >> $file1
-done
+#for (( n=17; n<33; n++ ))
+#do
+#	export OMP_NUM_THREADS=$n
+#	echo $n
+#	echo $n >> $file1
+#	numactl --physcpubind=0-15 ./$program | grep -E 'Copy:|Scale:|Add:|Triad:|Average:' | awk '{print $2}' >> $file1
+#done
 
 m=0
 templine=""
