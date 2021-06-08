59d2c5955be7954cbbb4311c79932f7540d053d4
Svein Gunnar Fagerheim <sveinfagerheim@gmail.com>
2021-06-08 20:45:53 +0200

    updates

--- IN4200_PE_15344/Calculation_only/PE_main_15344.c
+++ IN4200_PE_15344/Calculation_only/PE_main_15344.c
@@ -46,10 +46,12 @@ int main(int argc, char **argv){
 	transfer_threads = atof(argv[3]);
 	//max_threads = atof(argv[4]);
 	//printf("test 1\n");
+	
 	double test_time = mysecond();
-	printf("test\n");
+	//printf("test\n");
 	PageRank_iterations();
 	test_time = mysecond() - test_time;
+	
 	//printf("Iteration_threads transfer_threads max_threads time\n");
 	
 	printf("%d,%d,%d,%f,%f,%f\n", iter_threads+transfer_threads, iter_threads, transfer_threads, iteration_time, iteration_idle_time, test_time);
